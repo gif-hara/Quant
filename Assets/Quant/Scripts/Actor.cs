51de1d68a60b0c0cfee4242c42c092e822851e06
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-01-03 16:51:15 +0900

    :new: キャラクター適当に追加

---
+++ Assets/Quant/Scripts/Actor.cs
@@ -0,0 +1,29 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Quant
+{
+    /// <summary>
+    /// ゲーム中に使われるキャラクターにアタッチされるコンポーネント
+    /// </summary>
+    public sealed class Actor : MonoBehaviour
+    {
+        private Animator animator;
+
+        void Awake()
+        {
+            this.animator = this.GetComponent<Animator>();
+            this.UpdateAsObservable()
+                .SubscribeWithState(this, (_, _this) =>
+                {
+                    if (Input.GetKeyDown(KeyCode.A))
+                    {
+                        this.animator.SetTrigger("Attack");
+                        Debug.Log("?");
+                    }
+                });
+        }
+    }
+}
