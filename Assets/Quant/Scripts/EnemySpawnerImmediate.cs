09f12f26d756c931608cd6e33575b4962446a841
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-01-04 22:36:24 +0900

    :new: 即座に敵を生成できるように

---
+++ Assets/Quant/Scripts/EnemySpawnerImmediate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Quant
+{
+    /// <summary>
+    /// 即座に敵アクターを生成するクラス
+    /// </summary>
+    public sealed class EnemySpawnerImmediate : MonoBehaviour
+    {
+        [SerializeField]
+        private ActorSpawnParameter parameter;
+
+        void Start()
+        {
+            var t = this.transform;
+            this.parameter.Spawn(t.position, t.rotation, Layers.Id.Enemy);
+        }
+    }
+}
