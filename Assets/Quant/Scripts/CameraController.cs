9010228a4958c737bac644b85e8a00aede9983c9
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-01-05 15:13:18 +0900

    :new: ゲーム用のカメラ制御を行えるように

---
+++ Assets/Quant/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using HK.Framework.EventSystems;
+using Quant.Events;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Quant
+{
+    /// <summary>
+    /// ゲーム中のカメラを制御するクラス
+    /// </summary>
+    public sealed class CameraController : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Broker.Global.Receive<SpawnedPlayerActor>()
+                .SubscribeWithState(this, (x, _this) => _this.Setup(x.Actor))
+                .AddTo(this);
+        }
+
+        private void Setup(Actor actor)
+        {
+            this.FixedUpdateAsObservable()
+                .SubscribeWithState2(this, actor, (_, _this, _actor) =>
+                {
+                    var cameraman = Cameraman.Instance;
+                    cameraman.Position = _actor.transform.position;
+                });
+        }
+    }
+}
